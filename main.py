4c5d4ceded506f32b455ea9a6a3791b8f92d6586
Bartek Piguła <bartoszpigula@gmail.com>
2021-12-30 23:27:57 +0100

    Created classify method, not tested fully yet

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import sys
 import tkinter
-from typing import Union
-
 from future.moves.tkinter import filedialog
 from PySide2.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QStackedLayout, QLabel, \
     QGridLayout
 from PySide2.QtGui import QPixmap
 from PySide2.QtCore import Qt
 from PIL import Image
+import numpy as np
+from keras.models import load_model
 
 
 class MainWindow(QMainWindow):
@@ -39,14 +39,16 @@ class MainWindow(QMainWindow):
         self.layout2 = QGridLayout(self.page_2)
         self.displayed_image = QPixmap()
         self.image_label = QLabel(scaledContents=True)
-        self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setAlignment(Qt.AlignHCenter)
         self.classify_button = QPushButton("classify")
         self.another_button = QPushButton("another image")
+        self.another_button.clicked.connect(self.load_image)
+        self.classify_button.clicked.connect(self.classify_image)
         self.layout2.setRowMinimumHeight(0, 50)
         self.layout2.setRowMinimumHeight(1, 500)
         self.layout2.addWidget(self.classify_button, 0, 0, alignment=Qt.AlignCenter)
         self.layout2.addWidget(self.another_button, 0, 1, alignment=Qt.AlignCenter)
-        self.layout2.addWidget(self.image_label, 1, 0, 1, 2, alignment=Qt.AlignCenter)
+        self.layout2.addWidget(self.image_label, 1, 0, 1, 2, alignment=Qt.AlignHCenter)
 
         # setting up main layout
         self.main_layout.addWidget(self.page_1)
@@ -54,6 +56,9 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.central_widget)
         self.page_1.show()
         self.page_2.hide()
+
+        # neural net model
+        self.model = load_model('/home/bartek/PycharmProjects/image_classifier/net_model_0.667200.h5')
 
     def load_image(self):
         # get path of image with filedialog
@@ -63,11 +68,35 @@ class MainWindow(QMainWindow):
         image_path = filedialog.askopenfilename(initialdir='/home/bartek/Pictures')
         root_window.destroy()
         if image_path:
+            self.image_to_classify = Image.open(image_path)
             self.displayed_image.load(image_path)
+            self.image_label.clear()
             self.image_label.setPixmap(self.displayed_image)
-            self.image_label.setAlignment(Qt.AlignCenter)
             self.page_2.show()
             self.page_1.hide()
+
+    def prepare_image(self):
+        self.image_to_classify = self.image_to_classify.resize((32, 32))
+        self.image_to_classify = np.expand_dims(self.image_to_classify, axis=0)
+        self.image_to_classify = np.array(self.image_to_classify)
+
+    def classify_image(self):
+        class_dictionary = {
+            0: 'aeroplane',
+            1: 'automobile',
+            2: 'bird',
+            3: 'cat',
+            4: 'deer',
+            5: 'dog',
+            6: 'frog',
+            7: 'horse',
+            8: 'ship',
+            9: 'truck'
+        }
+        self.prepare_image()
+        prediction = self.model.predict([self.image_to_classify])
+        prediction = (prediction > 0.5).astype('int32')
+        print(prediction)
 
 
 if __name__ == '__main__':
